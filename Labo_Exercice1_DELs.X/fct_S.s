7c3372aaad5b091856f50f440d308481ce52fff7
Mat-desau <115102447+Mat-desau@users.noreply.github.com>
2024-02-07 15:56:55 -0500

    BOZOOOO PANIQUEEEE

--- Labo_Exercice1_DELs.X/fct_S.s
+++ Labo_Exercice1_DELs.X/fct_S.s
@@ -20,14 +20,14 @@ loop:
    
    # addi $t0, $t0, 1
    
-   lb $s0, 0($a0)
-   add $v0, $v0, $s0
-   addi $a0, $a0, 1
-   addi $t0, $t0, 1
+   lb $s0, 0($a0) # on load à l'adresse S0, la valeur à la postiion 0, 1 de a0 ensuite on add un pour voir position 0, 2 de a0 ect
+   add $v0, $v0, $s0 # on add la réponse à v0, puisque si ouvert c'est 1 et si fermer c'est 0
+   addi $a0, $a0, 1 # on augemente a0, pour se rendre à 8
+   addi $t0, $t0, 1 # on fait notre comtpeur
    
-   blt $t0, 8, loop 
-   nop
-   # la $v0, $s0 # Initialise la sortie à 2 (noLED)
+   blt $t0, 8, loop # on sort d'ici lorsque notre compteur est rendu à 7. car blt c'est plus petit donc 7 < 8. 
+   nop # necessaire pour le bon fonctionnement dont know fucking why though
+   
 
    jr $ra   # Retourne au Main
    nop	    # delay slot
--- Labo_Exercice2_Interrupt_timer.X/main.c
+++ Labo_Exercice2_Interrupt_timer.X/main.c
@@ -29,7 +29,8 @@ void initialize_timer_interrupt(void) {
   T1CONbits.TCKPS = 2;                //    ? prescaler value
   T1CONbits.TGATE = 0;                //    not gated input (the default)
   T1CONbits.TCS = 0;                  //    PCBLK input (the default)
-  PR1 = (int)(((float)(TMR_TIME * PB_FRQ) / 256) + 0.5);   //set period register, generates one interrupt every ? us
+  // PR1 = (int)(((float)(TMR_TIME * PB_FRQ) / 256) + 0.5); 
+  PR1 = 1000;  //set period register, generates one interrupt every ? us
   TMR1 = 0;                           //    initialize count to 0
   IPC1bits.T1IP = 2;                  //    INT step 4: priority
   IPC1bits.T1IS = 0;                  //    subpriority
@@ -49,7 +50,7 @@ void main() {
     while(1) {
         if(Flag_1s) {
             Flag_1s = 0;        // Reset the flag to capture the next event
-            if (++count >= 500) {
+            if (++count >= 1) {
                 count = 0;
                 LED_ToggleValue(0); // Toggle the led at 1 Hz
             }
